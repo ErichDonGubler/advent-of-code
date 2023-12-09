406a7e45abc5fe3c57cc76d5f69c6d85cba6f1b2
Erich Gubler <erichdongubler@gmail.com>
2023-12-09 15:36:02 -0500

    perf(d5): iterate over day 2 ranges, don't expand to full `Vec`s

--- tests/d5.rs
+++ tests/d5.rs
@@ -14,10 +14,10 @@ use nutype::nutype;
 mod spaces {
     use std::fmt::{self, Debug, Display, Formatter};
 
-    #[derive(Clone, Debug, Eq, PartialEq)]
+    #[derive(Clone, Copy, Debug, Eq, PartialEq)]
     pub(crate) struct Seed;
 
-    #[derive(Clone, Debug, Eq, PartialEq)]
+    #[derive(Clone, Copy, Debug, Eq, PartialEq)]
     pub(crate) struct Location;
 
     #[derive(Clone, Eq, PartialEq)]
@@ -81,36 +81,40 @@ mod spaces {
 #[nutype(derive(Clone, Copy, Debug, Display, Eq, Ord, PartialEq, PartialOrd))]
 struct RawId(u64);
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Clone, Debug, Eq, PartialEq)]
 struct Id<Space> {
     value: RawId,
     space: Space,
 }
 
-// impl<Space> Id<Space> {
-//     pub fn as_raw(&self) -> RawId {
-//         let Self { value, space: _ } = self;
-//         value.clone()
-//     }
-// }
-
-// impl<S1> Id<S1> {
-//     pub fn as_space<S2>(self) -> Id<S2>
-//     where
-//         S2: From<S1>,
-//     {
-//         let Self { value, space } = self;
-//         Id {
-//             space: space.into(),
-//             value,
-//         }
-//     }
-// }
-
 #[derive(Debug)]
 struct AlmanacConfig<'a> {
-    seeds: Vec<Id<spaces::Seed>>,
+    seeds: SeedProvider,
     maps: Vec<Map<spaces::Dynamic<'a>, spaces::Dynamic<'a>>>,
+}
+
+#[derive(Debug)]
+enum SeedProvider {
+    Day1(Vec<Id<spaces::Seed>>),
+    Day2(Vec<(Id<spaces::Seed>, u64)>),
+}
+
+impl SeedProvider {
+    pub fn iter(&self) -> impl Iterator<Item = Id<spaces::Seed>> + '_ {
+        let iter: Box<dyn Iterator<Item = Id<spaces::Seed>>> = match &self {
+            SeedProvider::Day1(seeds) => Box::new(seeds.iter().cloned()),
+            SeedProvider::Day2(seed_ranges) => {
+                Box::new(seed_ranges.iter().cloned().flat_map(|(start, size)| {
+                    let start = start.value.into_inner();
+                    (start..(start + size)).map(|value| Id {
+                        space: spaces::Seed,
+                        value: RawId::new(value),
+                    })
+                }))
+            }
+        };
+        iter
+    }
 }
 
 impl<'a> AlmanacConfig<'a> {
@@ -120,7 +124,7 @@ impl<'a> AlmanacConfig<'a> {
 
     fn parser<S>(seeds_marshaller: S) -> impl Parser<'a, &'a str, Self, extra::Err<Rich<'a, char>>>
     where
-        S: Parser<'a, &'a str, Vec<Id<spaces::Seed>>, extra::Err<Rich<'a, char>>>,
+        S: Parser<'a, &'a str, SeedProvider, extra::Err<Rich<'a, char>>>,
     {
         let parse_u64 = digits(10).to_slice().from_str::<u64>().unwrapped();
 
@@ -169,7 +173,7 @@ impl<'a> AlmanacConfig<'a> {
 
     fn new<S>(input: &'a str, seeds_parser: S) -> Self
     where
-        S: Parser<'a, &'a str, Vec<Id<spaces::Seed>>, extra::Err<Rich<'a, char>>>,
+        S: Parser<'a, &'a str, SeedProvider, extra::Err<Rich<'a, char>>>,
     {
         let Self { seeds, mut maps } = Self::parser(seeds_parser)
             .parse(input)
@@ -201,11 +205,11 @@ impl<'a> AlmanacConfig<'a> {
                     entries: _,
                 } = map;
                 assert_eq!(
-                to_space,
-                next_from_space,
-                "`{}` map is not continous with previous map; expected to map from `{to_space}`",
-                map.display_type(),
-            );
+                        to_space,
+                        next_from_space,
+                        "`{}` map is not continous with previous map; expected to map from `{to_space}`",
+                        map.display_type(),
+                        );
                 to_space = next_to_space;
             }
         }
@@ -228,37 +232,28 @@ impl<'a> AlmanacConfig<'a> {
                 .padded_by(inline_whitespace())
                 .repeated()
                 .at_least(1)
-                .collect(),
+                .collect()
+                .map(SeedProvider::Day1),
         )
     }
 
     pub fn new_part_2(input: &'a str) -> Self {
         let seed_range = Self::parse_u64()
+            .map(RawId::new)
+            .map(|value| Id {
+                value,
+                space: spaces::Seed,
+            })
             .then_ignore(inline_whitespace())
             .then(Self::parse_u64());
-        let range_to_vals = |(start, len)| {
-            (start..start + len)
-                .into_iter()
-                .map(RawId::new)
-                .map(|value| Id {
-                    value,
-                    space: spaces::Seed,
-                })
-        };
         Self::new(
             input,
             seed_range
-                .clone()
-                .map(range_to_vals)
-                .map(|vals| vals.collect::<Vec<_>>())
-                .foldl(
-                    inline_whitespace().ignore_then(seed_range).repeated(),
-                    |mut acc, range| {
-                        acc.extend(range_to_vals(range));
-                        acc
-                    },
-                )
-                .padded_by(inline_whitespace()),
+                .padded_by(inline_whitespace())
+                .repeated()
+                .at_least(1)
+                .collect()
+                .map(SeedProvider::Day2),
         )
     }
 
@@ -281,24 +276,26 @@ impl<'a> AlmanacConfig<'a> {
                     } = map;
                     entries
                         .binary_search_by(|entry| {
-                            let MapEntry {
-                                start_id_from,
-                                start_id_to: _,
-                                size,
-                            } = entry;
-                            let ord = match start_id_from.cmp(&translated_idx) {
-                                Ordering::Less
-                                    if start_id_from
-                                        .into_inner()
-                                        .checked_add(size.get().try_into().unwrap())
-                                        .unwrap()
-                                        > translated_idx.into_inner() =>
-                                {
-                                    Ordering::Equal
+                            fn compare(entry: &MapEntry, translated_idx: RawId) -> Ordering {
+                                let MapEntry {
+                                    start_id_from,
+                                    start_id_to: _,
+                                    size,
+                                } = entry;
+                                match start_id_from.cmp(&translated_idx) {
+                                    Ordering::Less
+                                        if start_id_from
+                                            .into_inner()
+                                            .checked_add(size.get().try_into().unwrap())
+                                            .unwrap()
+                                            > translated_idx.into_inner() =>
+                                    {
+                                        Ordering::Equal
+                                    }
+                                    ord => ord,
                                 }
-                                ord => ord,
-                            };
-                            ord
+                            }
+                            compare(entry, translated_idx)
                         })
                         .ok()
                         .map(|idx| {
